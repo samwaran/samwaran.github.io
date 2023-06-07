92035aa4d071e357fb8cc3b37158debeb3e7e770
samwaranb <>
2023-06-07 13:06:57 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+//var data = document.getElementById("data-light");
+var icon = document.getElementById("icon");
+icon.onclick = function () {
+    //data.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("dark-theme");
+    if (document.body.classList.contains("dark-theme")) {
+        icon.src = "assets/lamp-light.png";
+    } else {
+        icon.src = "assets/lamp-dark.png";
+
+    }
+}
